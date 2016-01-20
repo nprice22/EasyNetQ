1f3d27a2132e1d532d8515c6e5bf6930e663dbe7
nprice22 <nick.price22@gmail.com>
2016-01-19 20:53:37 -0800

    Copying files

---
+++ Source/EasyNetQ.Scheduler.Cassandra/Program.cs
@@ -0,0 +1,41 @@
+﻿using log4net.Config;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Topshelf;
+using Topshelf.ServiceConfigurators;
+
+namespace EasyNetQ.Scheduler.Cassandra
+{
+    public class Program
+    {
+        private static void Main()
+        {
+            XmlConfigurator.Configure();
+
+            HostFactory.Run(hostConfiguration =>
+            {
+                hostConfiguration.EnableServiceRecovery(serviceRecoveryConfiguration =>
+                {
+                    serviceRecoveryConfiguration.RestartService(delayInMinutes: 1); // On the first service failure, reset service after a minute
+                    serviceRecoveryConfiguration.SetResetPeriod(days: 0); // Reset failure count after every failure
+                });
+                hostConfiguration.RunAsLocalSystem();
+                hostConfiguration.SetDescription("EasyNetQ.Scheduler");
+                hostConfiguration.SetDisplayName("EasyNetQ.Scheduler");
+                hostConfiguration.SetServiceName("EasyNetQ.Scheduler");
+
+                hostConfiguration.Service<ISchedulerService>(ConfigureService);
+            });
+        }
+
+        private static void ConfigureService(ServiceConfigurator<ISchedulerService> s)
+        {
+            s.ConstructUsing(_ => SchedulerServiceFactory.CreateScheduler());
+            s.WhenStarted(service => service.Start());
+            s.WhenStopped(service => service.Stop());
+        }
+    }
+}
---
+++ Source/EasyNetQ.Scheduler.Cassandra/SchedulerService.cs
@@ -0,0 +1,134 @@
+using System;
+using EasyNetQ.SystemMessages;
+using EasyNetQ.Topology;
+using System.Transactions;
+
+namespace EasyNetQ.Scheduler
+{
+    public interface ISchedulerService
+    {
+        void Start();
+        void Stop();
+
+    }
+
+    public class SchedulerService : ISchedulerService
+    {
+        private const string schedulerSubscriptionId = "schedulerSubscriptionId";
+
+        private readonly IBus bus;
+        private readonly IEasyNetQLogger log;
+        private readonly IScheduleRepository scheduleRepository;
+        private readonly SchedulerServiceConfiguration configuration;
+
+        private System.Threading.Timer publishTimer;
+        private System.Threading.Timer purgeTimer;
+
+        public SchedulerService(
+            IBus bus, 
+            IEasyNetQLogger log, 
+            IScheduleRepository scheduleRepository, 
+            SchedulerServiceConfiguration configuration)
+        {
+            this.bus = bus;
+            this.scheduleRepository = scheduleRepository;
+            this.configuration = configuration;
+            this.log = log;
+        }
+
+        public void Start()
+        {
+            log.DebugWrite("Starting SchedulerService");
+            bus.Subscribe<ScheduleMe>(schedulerSubscriptionId, OnMessage);
+            bus.Subscribe<UnscheduleMe>(schedulerSubscriptionId, OnMessage);
+
+            publishTimer = new System.Threading.Timer(OnPublishTimerTick, null, 0, configuration.PublishIntervalSeconds * 1000);
+            purgeTimer = new System.Threading.Timer(OnPurgeTimerTick, null, 0, configuration.PurgeIntervalSeconds * 1000);
+        }
+
+        public void Stop()
+        {
+            log.DebugWrite("Stopping SchedulerService");
+            if (publishTimer != null)
+            {
+                publishTimer.Dispose();
+            }
+            if (purgeTimer != null)
+            {
+                purgeTimer.Dispose();
+            }
+            if (bus != null)
+            {
+                bus.Dispose();
+            }
+        }
+
+        public void OnMessage(ScheduleMe scheduleMe)
+        {
+            log.DebugWrite("Got Schedule Message");
+            scheduleRepository.Store(scheduleMe);
+        }
+
+        public void OnMessage(UnscheduleMe unscheduleMe)
+        {
+            log.DebugWrite("Got Unschedule Message");
+            scheduleRepository.Cancel(unscheduleMe);
+        }
+
+        public void OnPublishTimerTick(object state)
+        {
+            if (!bus.IsConnected) return;
+            try
+            {
+                using(var scope = new TransactionScope())
+                {
+                    var scheduledMessages = scheduleRepository.GetPending();
+
+                    foreach (var scheduledMessage in scheduledMessages)
+                    {
+                        // Binding key fallback is only provided here for backwards compatibility, will be removed in the future
+                        log.DebugWrite(string.Format(
+                            "Publishing Scheduled Message with Routing Key: '{0}'", scheduledMessage.BindingKey));
+
+                        var exchangeName = scheduledMessage.Exchange ?? scheduledMessage.BindingKey;
+                        var exchangeType = scheduledMessage.ExchangeType ?? ExchangeType.Topic;
+
+                        var exchange = bus.Advanced.ExchangeDeclare(exchangeName, exchangeType);
+                        var messageProperties = scheduledMessage.MessageProperties;
+
+                        if (scheduledMessage.MessageProperties == null)
+                            messageProperties = new MessageProperties { Type = scheduledMessage.BindingKey };
+
+                        var routingKey = scheduledMessage.RoutingKey ?? scheduledMessage.BindingKey;
+
+                        bus.Advanced.Publish(
+                            exchange,
+                            routingKey,
+                            false,
+                            false,
+                            messageProperties,
+                            scheduledMessage.InnerMessage);
+                    }
+
+                    scope.Complete();
+                }
+            }
+            catch (Exception exception)
+            {
+                log.ErrorWrite("Error in schedule pol\r\n{0}", exception);
+            }
+        }
+
+        private void OnPurgeTimerTick(object state)
+        {
+            try
+            {
+                scheduleRepository.Purge();
+            }
+            catch (Exception exception)
+            {
+                log.ErrorWrite("Error in schedule purge\r\n{0}", exception);
+            }
+        }
+    }
+}
