fed2947b5fb077c0280e00f4bb22811ad0fa9572
nprice22 <nick.price22@gmail.com>
2016-01-19 22:49:50 -0800

    Setting up cassandra session.

--- Source/EasyNetQ.Scheduler.Cassandra/ScheduleRepository.cs
+++ Source/EasyNetQ.Scheduler.Cassandra/ScheduleRepository.cs
@@ -5,6 +5,7 @@ using System.Data.Common;
 using System.Threading;
 using EasyNetQ.SystemMessages;
 using Newtonsoft.Json;
+using Cassandra;
 
 namespace EasyNetQ.Scheduler
 {
@@ -20,89 +21,103 @@ namespace EasyNetQ.Scheduler
     {
         private readonly ScheduleRepositoryConfiguration configuration;
         private readonly Func<DateTime> now;
-        private readonly ISqlDialect dialect;
+        private ISession session;
 
         public ScheduleRepository(ScheduleRepositoryConfiguration configuration, Func<DateTime> now)
         {
             this.configuration = configuration;
             this.now = now;
-            this.dialect = SqlDialectResolver.Resolve(configuration.ProviderName);
+            this.session = GetClusterConnection(configuration.ConnectionString);
         }
 
         public void Store(ScheduleMe scheduleMe)
         {
-            
+            //WithStoredProcedureCommand(dialect.InsertProcedureName, command =>
+            //{
+            //    AddParameter(command, dialect.WakeTimeParameterName, scheduleMe.WakeTime, DbType.DateTime);
+            //    AddParameter(command, dialect.BindingKeyParameterName, scheduleMe.BindingKey, DbType.String);
+            //    AddParameter(command, dialect.CancellationKeyParameterName, scheduleMe.CancellationKey, DbType.String);
+            //    AddParameter(command, dialect.MessageParameterName, scheduleMe.InnerMessage, DbType.Binary);
+            //    AddParameter(command, dialect.ExchangeParameterName, scheduleMe.Exchange, DbType.String);
+            //    AddParameter(command, dialect.ExchangeTypeParameterName, scheduleMe.ExchangeType, DbType.String);
+            //    AddParameter(command, dialect.RoutingKeyParameterName, scheduleMe.RoutingKey, DbType.String);
+            //    AddParameter(command, dialect.MessagePropertiesParameterName, SerializeToString(scheduleMe.MessageProperties), DbType.String);
+            //    AddParameter(command, dialect.InstanceNameParameterName, configuration.InstanceName, DbType.String);
+
+            //    command.ExecuteNonQuery();
+            //});
         }
 
         public void Cancel(UnscheduleMe unscheduleMe)
         {
-            ThreadPool.QueueUserWorkItem(state =>
-                WithStoredProcedureCommand(dialect.CancelProcedureName, command =>
-                {
-                    AddParameter(command, dialect.CancellationKeyParameterName, unscheduleMe.CancellationKey, DbType.String);
+            //ThreadPool.QueueUserWorkItem(state =>
+            //    WithStoredProcedureCommand(dialect.CancelProcedureName, command =>
+            //    {
+            //        AddParameter(command, dialect.CancellationKeyParameterName, unscheduleMe.CancellationKey, DbType.String);
     
-                    command.ExecuteNonQuery();
-                })
-            );
+            //        command.ExecuteNonQuery();
+            //    })
+            //);
         }
 
         public IList<ScheduleMe> GetPending()
         {
-            var scheduledMessages = new List<ScheduleMe>();
-            var scheduleMessageIds = new List<int>();
+            //var scheduledMessages = new List<ScheduleMe>();
+            //var scheduleMessageIds = new List<int>();
 
-            WithStoredProcedureCommand(dialect.SelectProcedureName, command =>
-            {
-                var dateTime = now();
-                AddParameter(command, dialect.RowsParameterName, configuration.MaximumScheduleMessagesToReturn, DbType.Int32);
-                AddParameter(command, dialect.StatusParameterName, 0, DbType.Int16);
-                AddParameter(command, dialect.WakeTimeParameterName, dateTime, DbType.DateTime);
-                AddParameter(command, dialect.InstanceNameParameterName, configuration.InstanceName ?? "", DbType.String);
+            //WithStoredProcedureCommand(dialect.SelectProcedureName, command =>
+            //{
+            //    var dateTime = now();
+            //    AddParameter(command, dialect.RowsParameterName, configuration.MaximumScheduleMessagesToReturn, DbType.Int32);
+            //    AddParameter(command, dialect.StatusParameterName, 0, DbType.Int16);
+            //    AddParameter(command, dialect.WakeTimeParameterName, dateTime, DbType.DateTime);
+            //    AddParameter(command, dialect.InstanceNameParameterName, configuration.InstanceName ?? "", DbType.String);
 
-                using (var reader = command.ExecuteReader())
-                {
-                    while (reader.Read())
-                    {
-                        scheduledMessages.Add(new ScheduleMe
-                        {
-                            WakeTime = (DateTime) reader["WakeTime"],
-                            BindingKey = reader["BindingKey"].ToString(),
-                            InnerMessage = (byte[])reader["InnerMessage"],
-                            CancellationKey = reader["CancellationKey"].ToString(),
-                            Exchange = reader["Exchange"].ToString(),
-                            ExchangeType = reader["ExchangeType"].ToString(),
-                            RoutingKey = reader["RoutingKey"].ToString(),
-                            MessageProperties = DeserializeToMessageProperties(reader["MessageProperties"].ToString()),
-                        });
+            //    using (var reader = command.ExecuteReader())
+            //    {
+            //        while (reader.Read())
+            //        {
+            //            scheduledMessages.Add(new ScheduleMe
+            //            {
+            //                WakeTime = (DateTime) reader["WakeTime"],
+            //                BindingKey = reader["BindingKey"].ToString(),
+            //                InnerMessage = (byte[])reader["InnerMessage"],
+            //                CancellationKey = reader["CancellationKey"].ToString(),
+            //                Exchange = reader["Exchange"].ToString(),
+            //                ExchangeType = reader["ExchangeType"].ToString(),
+            //                RoutingKey = reader["RoutingKey"].ToString(),
+            //                MessageProperties = DeserializeToMessageProperties(reader["MessageProperties"].ToString()),
+            //            });
 
-                        scheduleMessageIds.Add((int)reader["WorkItemId"]);
-                    }
-                }
-            });
+            //            scheduleMessageIds.Add((int)reader["WorkItemId"]);
+            //        }
+            //    }
+            //});
 
-            MarkItemsForPurge(scheduleMessageIds);
+            //MarkItemsForPurge(scheduleMessageIds);
 
-            return scheduledMessages;
+            //return scheduledMessages;
+            throw new NotImplementedException();
         }
 
         public void MarkItemsForPurge(IEnumerable<int> scheduleMessageIds)
         {
             // mark items for purge on a background thread.
-            ThreadPool.QueueUserWorkItem(state =>
-                WithStoredProcedureCommand(dialect.MarkForPurgeProcedureName, command =>
-                {
-                    var purgeDate = now().AddDays(configuration.PurgeDelayDays);
+            //ThreadPool.QueueUserWorkItem(state =>
+            //    WithStoredProcedureCommand(dialect.MarkForPurgeProcedureName, command =>
+            //    {
+            //        var purgeDate = now().AddDays(configuration.PurgeDelayDays);
 
-                    var idParameter = AddParameter(command, dialect.IdParameterName, DbType.Int32);
-                    AddParameter(command, dialect.PurgeDateParameterName, purgeDate, DbType.DateTime);
+            //        var idParameter = AddParameter(command, dialect.IdParameterName, DbType.Int32);
+            //        AddParameter(command, dialect.PurgeDateParameterName, purgeDate, DbType.DateTime);
 
-                    foreach (var scheduleMessageId in scheduleMessageIds)
-                    {
-                        idParameter.Value = scheduleMessageId;
-                        command.ExecuteNonQuery();
-                    }
-                })
-            );
+            //        foreach (var scheduleMessageId in scheduleMessageIds)
+            //        {
+            //            idParameter.Value = scheduleMessageId;
+            //            command.ExecuteNonQuery();
+            //        }
+            //    })
+            //);
         }
 
         private static MessageProperties DeserializeToMessageProperties(string properties)
@@ -122,24 +137,15 @@ namespace EasyNetQ.Scheduler
 
         public void Purge()
         {
-            WithStoredProcedureCommand(dialect.PurgeProcedureName, command =>
-                {
-                    AddParameter(command, dialect.RowsParameterName, configuration.PurgeBatchSize, DbType.Int16);
-                    AddParameter(command, dialect.PurgeDateParameterName, now(), DbType.DateTime);
+            //WithStoredProcedureCommand(dialect.PurgeProcedureName, command =>
+            //    {
+            //        AddParameter(command, dialect.RowsParameterName, configuration.PurgeBatchSize, DbType.Int16);
+            //        AddParameter(command, dialect.PurgeDateParameterName, now(), DbType.DateTime);
 
-                    command.ExecuteNonQuery();
-                });
+            //        command.ExecuteNonQuery();
+            //    });
         }
 
-        private void WithStoredProcedureCommand(string storedProcedureName, Action<IDbCommand> commandAction)
-        {
-            using (var connection = GetConnection())
-            using (var command = CreateCommand(connection, FormatWithSchemaName(storedProcedureName)))
-            {
-                command.CommandType = CommandType.StoredProcedure;
-                commandAction(command);
-            }
-        }
 
         private string FormatWithSchemaName(string storedProcedureName)
         {
@@ -149,13 +155,17 @@ namespace EasyNetQ.Scheduler
             return string.Format("[{0}].{1}", configuration.SchemaName.TrimStart('[').TrimEnd('.', ']'), storedProcedureName);
         }
 
-        private IDbConnection GetConnection()
+        private ISession GetClusterConnection(string node)
         {
-            var factory = DbProviderFactories.GetFactory(configuration.ProviderName);
-            var connection = factory.CreateConnection();
-            connection.ConnectionString = configuration.ConnectionString;
-            connection.Open();
-            return connection;
+            var scheduleCluster = Cluster.Builder().AddContactPoint(node).Build();
+            Console.WriteLine("Connected to cluster: " + scheduleCluster.Metadata.ClusterName.ToString());
+            foreach (var host in scheduleCluster.Metadata.AllHosts())
+            {
+                Console.WriteLine("Data Center: " + host.Datacenter + ", " +
+                    "Host: " + host.Address + ", " +
+                    "Rack: " + host.Rack);
+            }
+            return scheduleCluster.Connect();
         }
 
         private IDbCommand CreateCommand(IDbConnection connection, string commandText)
